861c4ef647ad914832fdddb84eaa25e77f2e216e
ReynaldoSG <124290617+ReynaldoSG@users.noreply.github.com>
2023-10-15 01:51:41 -0600

    ProyectoFinal-1erParcial

--- Proyecto1/src/app/app.module.ts
+++ Proyecto1/src/app/app.module.ts
@@ -1,18 +1,61 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Importaciones de componentes
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatCardModule} from '@angular/material/card';
+
+
+import {RouterModule, Routes} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InicioComponent } from './inicio/inicio.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { LoginComponent } from './login/login.component';
+
+//Cuando se agrega una nueva pagina al sitio se agrega aqui
+const appRoutes: Routes=[
+  {path:'inicio', component:InicioComponent},
+  {path:'nosotros', component:NosotrosComponent},
+  {path:'login', component:LoginComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NosotrosComponent,
+    LoginComponent,
   ],
   imports: [
+    RouterModule.forRoot(appRoutes,{enableTracing: true}),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatChipsModule,
+    MatDividerModule,
+    MatTabsModule,
+    MatCardModule
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  exports: [
+    RouterModule
+  ],
+  providers: [
+
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {
+}
